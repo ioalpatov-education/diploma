019e71dca309cf92db06ba24afc1353ebe637af5
IlyaAlpatov <alpatov30@gmail.com>
2023-06-05 22:29:27 +0300

    добавлен тестовый роутинг

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,36 @@
 import "./App.css";
+import HomePage from "./views/HomePage";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { useRoutes } from "react-router-dom";
 
 function App() {
-  return <div className="app"></div>;
+  const routes = useRoutes([
+    {
+      path: "/",
+      element: <HomePage />,
+    },
+    {
+      path: "/catalog",
+      element: <HomePage />,
+    },
+    {
+      path: "/about",
+      element: <HomePage />,
+    },
+    {
+      path: "/contacts",
+      element: <HomePage />,
+    },
+  ]);
+
+  return (
+    <div className="app">
+      <Header />
+      <main className="container"> {routes}</main>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
